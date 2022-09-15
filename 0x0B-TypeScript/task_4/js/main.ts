a1517d0d0d44eb68a1f043518b50fec2a8446e73
Solienne <soliene.teixeira@ufba.br>
2022-09-15 17:32:32 +0000

    type

--- 0x0B-TypeScript/task_4/js/main.ts
+++ 0x0B-TypeScript/task_4/js/main.ts
@@ -4,9 +4,9 @@
 /// <reference path="./subjects/Subject.ts" />
 
 
-const Cpp: Subjects.Cpp = {new Subjects.Cpp();};
-const Java: Subjects.Java = {new Subjects.Java();};
-const React: Subjects.React = {new Subjects.React();};
+const Cpp: Subjects.Cpp = new Subjects.Cpp();
+const Java: Subjects.Java = new Subjects.Java();
+const React: Subjects.React = new Subjects.React();
 
 Subjects.TeacherInterface = {
   firstName: 'Guillaume',
