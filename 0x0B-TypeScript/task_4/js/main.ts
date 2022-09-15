b4e6128efc43f91c0d17e458861d15f74f8645e2
Solienne <soliene.teixeira@ufba.br>
2022-09-15 16:30:17 +0000

    trying

--- 0x0B-TypeScript/task_4/js/main.ts
+++ 0x0B-TypeScript/task_4/js/main.ts
@@ -1,7 +1,6 @@
 /// <reference path="./subjects/Cpp.ts" />
 /// <reference path="./subjects/Java.ts" />
 /// <reference path="./subjects/React.ts" />
-/// <reference path="./subjects/Subject.ts" />
 
 
 const cpp: Subjects.Cpp = new Subjects.Cpp();
