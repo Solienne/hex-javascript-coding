62d001591e7ee443437ee8c6bf877deeb9bc8cbb
Solienne <soliene.teixeira@ufba.br>
2022-09-16 14:14:23 +0000

    trying

--- 0x0B-TypeScript/task_4/js/main.ts
+++ 0x0B-TypeScript/task_4/js/main.ts
@@ -1,4 +1,4 @@
-import {Cpp as C++} fom "./subjects/Cpp";
+import {Cpp} fom "./subjects/Cpp";
 import {Java as Java} from "./subjects/Java";
 import {React as React} from "./subjects/React";
 import {Subject as Subject} from "./subjects/subject";
