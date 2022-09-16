1ac6d3ab0e814538c053280a653580f92fdbe150
Solienne <soliene.teixeira@ufba.br>
2022-09-16 02:21:52 +0000

    adding 'as'

--- 0x0B-TypeScript/task_4/js/main.ts
+++ 0x0B-TypeScript/task_4/js/main.ts
@@ -8,24 +8,21 @@ const cpp: Subjects.Cpp = new Subjects.Cpp();
 const java: Subjects.Java = new Subjects.Java();
 const react: Subjects.React = new Subjects.React();
 
-Subjects.TeacherInterface = {
+export const cTeachet : Subjects.Teacher = {
   firstName: "Guillaume",
   lastName: "Salva",
   experienceTeachingC: 10,
 };
 
 console.log('C++');
-cpp.setTeacher(cTeacher);
-console.log(cpp.getRequirements());
-console.log(cpp.getAvailableTeacher());
+cpp.setTeacher = cTeacher;
+console.log(cpp.getRequirements(), console.log cpp.getAvailableTeacher());
 
 console.log('Java');
-java.setTeacher(cTeacher);
-console.log(java.getRequirements());
-console.log(java.getAvailableTeacher());
+java.setTeacher = cTeacher;
+console.log(java.getRequirements(), java.getAvailableTeacher());
 
 console.log('React');
-react.setTeacher(cTeacher);
-console.log(react.getRequirements());
-console.log(react.getAvailableTeacher());
+react.setTeacher = cTeacher;
+console.log(react.getRequirements(), :wreact.getAvailableTeacher());
 
